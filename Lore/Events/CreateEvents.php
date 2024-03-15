a5dd403409c2ddd6e7bed06ad16bacb1605e4777
Julia Günther <nakahara.kyoko16@gmail.com>
2024-03-15 14:08:31 +0100

    Create Event Base

--- Lore/Events/CreateEvents.php
+++ Lore/Events/CreateEvents.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Erstelle ein Lore-Event</title>
+    <link rel="stylesheet" href="../../index.css">
+    <link rel="stylesheet" href="../Lore.css">
+    <link rel="stylesheet" href="../../Characters/Edit/Edit.css">
 </head>
+
 <body>
-    
+    <div class="background-image"></div>
+    <div class="div-2">
+        <?php include("../../GlobalResources/Navbar.php") ?>
+        <div class="flex">
+            <h1>Erstelle ein Event</h1>
+            <form action="CreateEvents.php" method="post">
+                <label for="Name">Name:</label>
+                <input type="text" name="Name" id="Name">
+            </form>
+            <div class="spacer">
+                <input type="hidden" name="">
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
+
+<style>
+    h1 {
+        margin: 20px
+    }
+</style>
